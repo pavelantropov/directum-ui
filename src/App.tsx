032edce3df3f43f249bae646e731dee3ecda6c16
Pavel Antropov <pavel_antropov@epam.com>
2022-05-15 00:09:11 +0400

    Develop the Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Paper } from "@mui/material";
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
+import Text from "./components/Text/Text";
 
 function App() {
 
   return (
     <>
       <Banner />
+      <Paper square sx={{
+        textAlign: "center",
+        bgcolor: "#F8F9FA",
+        padding: "2rem 0 2rem 0"
+      }}>
+        <Text />
+      </Paper>
       <Footer />
     </>
   );
