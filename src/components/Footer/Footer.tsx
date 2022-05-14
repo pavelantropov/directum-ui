dd0ec44c3cd28529364a59b4c90b81c215c73aa6
Pavel Antropov <pavel_antropov@epam.com>
2022-05-14 23:52:41 +0400

    Update Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,23 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import logoFooter from "../../images/logoFooter.png";
 
 function Footer() {
   return (
     <>
-      <Box
-          sx={{
-            position: "fixed",
-            bottom: 0,
-            width: "100%",
-          }}
-        >
-        <Paper square sx={{
-          textAlign: "center", 
-          color: "#BABABA",
-          bgcolor: "#F8F9FA"
-        }}>
-          <img src={logoFooter} style={{padding: "20px 0 5px 0"}} />
-          <Typography paddingBottom={3}>
-            <small>© DIRECTUM, 2022</small>
-          </Typography>
-        </Paper>
-      </Box>
+      <Paper square sx={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        textAlign: "center", 
+        color: "#BABABA",
+        bgcolor: "#F8F9FA",
+        padding: "2rem 0 2rem 0"
+      }}>
+        <img src={logoFooter} />
+        <Typography>
+          <small>© DIRECTUM, 2022</small>
+        </Typography>
+      </Paper>
     </>
   );
 }
